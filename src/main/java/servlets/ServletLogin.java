209cb07fcd3eab51262584c4d9eadff67576bda6
Pedro Henrique <pedro.henrique59@outlook.com>
2023-04-18 16:25:26 -0300

    Ajustando ServletLogin e excluindo página jsp.

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "servletLogin", value = "/servletLogin")
+@WebServlet("/servletLogin")
 public class ServletLogin extends HttpServlet {
 
     public ServletLogin() {
