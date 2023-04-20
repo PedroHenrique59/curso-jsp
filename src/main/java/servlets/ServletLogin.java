a19eac0f38e95182bd82a2018345f1595a4294b0
Pedro Henrique <pedro.henrique59@outlook.com>
2023-04-20 17:37:15 -0300

    Adicionando dashboard.

--- src/main/java/servlets/ServletLogin.java
+++ src/main/java/servlets/ServletLogin.java
@@ -22,6 +22,7 @@ public class ServletLogin extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
     }
 
     @Override
